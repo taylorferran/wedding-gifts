76261ad2a870613fe82e0ad1b3c17c4d03bb24fc
taylorferran <tailzpk@hotmail.co.uk>
2022-11-27 19:57:58 +0800

    first commit

---
+++ hardhat/contracts/WeddingGifts.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.16;
+
+contract WeddingGifts {
+
+    mapping (address => uint256) public coupleBalance;
+
+    function createCouple(string memory _coupleName) public {
+        
+    }
+
+}
